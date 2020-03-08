e1ac45221ae345483298678120f9226822364d87
zhangfan <sa517486@mail.ustc.edu.cn>
2020-03-08 22:54:41 +0800

    提交单元测试

---
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,48 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * 配置spring与Junit的整合
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+
+public class SeckillDaoTest {
+
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void testQueryById() throws Exception{
+        long id = 1000;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+    @Test
+    public void testReduceNumber() {
+    }
+
+    @Test
+    public void queryAll() {
+        List<Seckill> seckills = seckillDao.queryAll(0,100);
+        for (Seckill seckill : seckills){
+            System.out.println(seckill);
+        }
+    }
+}
+
+
